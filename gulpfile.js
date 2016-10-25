d7a8bdcfcdd031c7ec9b7065500eefe98e89f363
joje <joje.attrs@gmail.com>
2016-10-25 21:24:31 +0900

    add gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -7,27 +7,18 @@ const rename = require("gulp-rename");
 const uglify = require('gulp-uglify');
 const rimraf = require('gulp-rimraf');
 const header = require('gulp-header');
-const webpack = require('webpack-stream');
-
+const less = require('gulp-less');
+const cssmin = require('gulp-cssmin');
 const pkg = require('./package.json');
-const dist = path.join(__dirname, 'dist');
 
-gulp.task('build.js.clean', () => {
+gulp.task('build.clean', () => {
   return gulp.src('dist', { read: false })
     .pipe(rimraf());
 });
 
-gulp.task('build.webpack', ['build.js.clean'], () => {
-  var conf = require('./webpack.config.js');
-  delete conf.output.path;
-  
-  return gulp.src('lib/index.js')
-    .pipe(webpack(conf))
-    .pipe(gulp.dest(dist));
-});
-
-gulp.task('build', ['build.webpack'], () => {
-  return gulp.src(path.join(dist, 'firefront.js'))
+gulp.task('build', ['build.clean'], () => {
+  return gulp.src('less/index.less')
+    .pipe(less())
     .pipe(header([
       '/*!',
       '* <%= pkg.name %>',
@@ -39,13 +30,16 @@ gulp.task('build', ['build.webpack'], () => {
       '*/',
       ''
     ].join('\n'), { pkg: pkg }))
-    .pipe(gulp.dest(dist))
-    .pipe(uglify())
+    .pipe(rename({
+      basename: pkg.name
+    }))
+    .pipe(gulp.dest('dist'))
+    .pipe(cssmin())
     .pipe(header('/*! <%= pkg.name %> - attrs */', { pkg: pkg }))
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest(dist));
+    .pipe(gulp.dest('dist'));
 });
 
 // conclusion
